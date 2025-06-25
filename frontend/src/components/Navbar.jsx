287c4525b0170d5e1c5a386ccf5307cc5e25a758
Naman Patel <namanp2166@gmail.com>
2025-06-25 16:25:05 +0530

    increases responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,72 +11,72 @@ const Navbar = () => {
         <div className="w-full">
 
             {/* Top Navbar */}
-            <div className='lg:flex lg:flex-row  bg-[#1245A8] text-sm'>
+            <div className='xl:flex xl:flex-row bg-[#1245A8] text-sm'>
 
-                <div className='flex justify-between items-center h-20 bg-white px-4 py-3 pr-16 lg:[clip-path:polygon(0_0,100%_0,90%_100%,0%_100%)]'>
+                <div className='flex justify-between items-center h-16 sm:h-20 bg-white px-2 lg:px-4 py-3 pr-6 lg:pr-16 xl:[clip-path:polygon(0_0,100%_0,90%_100%,0%_100%)]'>
 
                     {/* Left - college Logo and Info */}
-                    <div className='flex justify-center items-center gap-3'>
-                        <img className='w-14 h-auto cursor-pointer' src={assets.college_logo} alt='' />
+                    <div className='flex justify-center items-center gap-1 md:gap-3'>
+                        <img className='w-10 md:w-14 h-auto cursor-pointer' src={assets.college_logo} alt='' />
 
-                        <div className='flex flex-col items-start justify-center text-[6px] lg:text-[6.5px]'>
-                            <p className='text-red-600 text-[8.5px] lg:text-[9px] font-semibold'>Sri Balaji Society's</p>
-                            <p className='text-orange-600 text-[10px] lg:text-[10.5px] font-semibold'>Balaji Law College (BLC) - Pune</p>
+                        <div className='flex flex-col items-start justify-center text-[5.5px] sm:text-[6px] lg:text-[6.5px]'>
+                            <p className='text-red-600 text-[7px] sm:text-[8.5px] lg:text-[9px] font-semibold'>Sri Balaji Society's</p>
+                            <p className='text-orange-600 text-[8.5px] sm:text-[10px] lg:text-[10.5px] font-semibold'>Balaji Law College (BLC) - Pune</p>
                             <p className='font-medium font-semibold'>Affiliated to SavitriBai Phule Pune University, Recognised <br /> by Government of Maharastra & Bar Council of India</p>
-                            <p className='text-blue-900 text-[9px] lg:text-[9.5px] font-semibold'>NAAC Accredited <span className='mx-1'> | </span> AISHE CODE : C-42094</p>
+                            <p className='text-blue-900 text-[8px] sm:text-[9px] lg:text-[9.5px] font-semibold'>NAAC Accredited <span className='sm:mx-1'> | </span> AISHE CODE : C-42094</p>
                         </div>
 
                         {/* vertical line */}
-                        <div className="border-l-1  border-gray-400 h-10"></div>
+                        <div className="border-l-1 border-gray-400 h-9 md:h-10"></div>
 
                         {/* university logo */}
-                        <img className='w-14 h-auto cursor-pointer ml-2 lg:ml-5' src={assets.university_logo} alt='' />
+                        <img className='w-10 sm:w-14 h-auto cursor-pointer ml-2 lg:ml-5' src={assets.university_logo} alt='' />
                     </div>
 
-                    {/* Right - University Logo */}
-                    <div>
+                    
+                    
                         {/* Hamburger icon for mobile */}
-                        <div className="lg:hidden flex items-center mr-2">
-                            <FaBars onClick={() => setShowMenu(!showMenu)} className="text-4xl text-[#1245A8] cursor-pointer" />
+                        <div className="xl:hidden flex items-center mr-0 md:mr-2">
+                            <FaBars onClick={() => setShowMenu(!showMenu)} className="text-3xl md:text-4xl text-[#1245A8] cursor-pointer" />
                         </div>
-                    </div>
+                    
 
                 </div>
 
 
                 {/* Desktop Menu */}
-                <div className='hidden lg:flex  items-center  gap-16 py-3 px-7 mx-auto bg-[#1245A8]'>
-                    <ul className='text-white text-[16px] flex justify-between  gap-5 lg:gap-13 font-bold'>
+                <div className='hidden xl:flex flex-row items-center gap-16 py-3 px-7 mx-auto bg-[#1245A8]'>
+                    <ul className='text-white text-[16px] lg:text-[14px] xl:text-[16px] flex justify-between gap-5 xl:gap-13 font-bold'>
                         <NavLink to='/' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Home</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Home</li>
                         </NavLink>
                         <NavLink to='/about-us' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>About Us</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>About Us</li>
                         </NavLink>
                         <NavLink to='/admission' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Admission</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Admission</li>
                         </NavLink>
                         <NavLink to='/academics' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Academics</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Academics</li>
                         </NavLink>
                         <NavLink to='/library' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Library</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Library</li>
                         </NavLink>
                         <NavLink to='/student-corner' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Student Corner</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Student Corner</li>
                         </NavLink>
                         <NavLink to='/gallery' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Gallery</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Gallery</li>
                         </NavLink>
                         <NavLink to='/contact-us' className="text-decoration-none flex flex-col items-center hover:scale-105">
-                            <li className='py-1'>Contact Us</li>
+                            <li className='py-1 hover:text-black font-bold whitespace-nowrap'>Contact Us</li>
                         </NavLink>
                     </ul>
                 </div>
 
                 {/* Mobile Menu (only when showMenu is true) */}
                 {showMenu && (
-                    <div className='lg:hidden flex flex-col items-start gap-4 bg-[#1245A8] text-white px-6 py-4 font-semibold text-[15px]'>
+                    <div className='xl:hidden flex flex-col items-start gap-4 bg-[#1245A8] text-white px-6 py-4 font-semibold text-[15px]'>
                         <NavLink to='/' onClick={() => setShowMenu(false)} className="hover:text-black font-bold " >Home</NavLink>
                         <NavLink to='/about-us' onClick={() => setShowMenu(false)} className="hover:text-black font-bold" >About Us</NavLink>
                         <NavLink to='/admission' onClick={() => setShowMenu(false)} className="hover:text-black font-bold" >Academics</NavLink>
--- frontend/src/components/home/HomeHeader.jsx
+++ frontend/src/components/home/HomeHeader.jsx
@@ -7,23 +7,23 @@ import { FaSearch, FaArrowRight } from "react-icons/fa";
 const HomeHeader = () => {
   return (
     <div
-      className='flex flex-col md:flex-row flex-wrap bg-cover bg-center px-6 md:px-10 lg:px-20 min-h-[671px]'
+      className='flex flex-col md:flex-row flex-wrap bg-cover bg-center px-6 md:px-10 lg:px-20 min-h-auto lg:min-h-[671px]'
       style={{ backgroundImage: `url(${assets.HomeHeaderBgImageCropped})` }} >
 
 
       {/* -------------- Left Side  -------------- */}
-      <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 md:py-[8vw] '>
+      <div className='w-1/2  flex flex-col items-start justify-center gap-4 py-10 md:py-[8vw] '>
 
-        <p className='mx-auto md:mx-0 text-3xl md:text-4xl lg:text-5xl text-amber-800 font-medium leading-tight md:leading-tight lg:leading-tight '>
+        <p className='mx-0 text-3xl md:text-4xl lg:text-6xl text-amber-800 font-bold leading-tight md:leading-tight lg:leading-tight '>
           Pursue a <br/> Career in Law
         </p>
         
 
         {/* Search Bar */}
-        <p className='bg-orange-600 text-white px-4 py-2 rounded-md'>Admission Open</p>
+        <p className='bg-orange-600 text-white text-sm md:text-md px-2 md:px-4 py-1.5 md:py-2.5 rounded-md'>Admission Open</p>
   
         
-        <p className='text-3xl'>for <span className='font-bold'>B.A.LL.B</span> & <span className='font-bold'>LL.B</span>  </p>
+        <p className='text-xl md:text-4xl'>for <span className='font-bold'>B.A.LL.B</span> & <span className='font-bold'>LL.B</span>  </p>
         
 
       </div>
